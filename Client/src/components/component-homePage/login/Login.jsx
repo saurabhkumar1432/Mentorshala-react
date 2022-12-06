a0bbf205935102412a85a876b0f7c3e361da4f69
Anant <chaturvedi.a20@iiits.in>
2022-12-07 01:09:06 +0530

    login ui

--- Client/src/components/component-homePage/login/Login.jsx
+++ Client/src/components/component-homePage/login/Login.jsx
@@ -1,61 +1,94 @@
-import React from 'react'
-import { useState, useCallback, useEffect } from 'react';
-import './login.css'
-// import loginImg from '../../assets/logo.svg'
-import logInimg from '../../../images/22866003-removebg-preview.png'
+import React from "react";
+import { useState, useCallback, useEffect } from "react";
+import "./login.css";
+import Wave from "../../../images/wave.png";
+import Bg from "../../../images/bg.svg";
+import Avatar from "../../../images/avatar.svg";
+import logInimg from "../../../images/22866003-removebg-preview.png";
 const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [found, setFound] = useState(false);
-    const handleChange = useCallback(async () => {
-        try {
-            const response = await fetch('https://react-http-1-2a5c9-default-rtdb.firebaseio.com/movies.json');
-            if (!response.ok) {
-                throw new Error('Something went wrong!');
-            }
-            const data = await response.json();
-            const loadedMovies = [];
-            console.log(data);
-            data.array.forEach(element => {
-                if (element.email === email) {
-                    setFound(true);
-                }
-            });
-            console.log(found)
-            if(found===false){
-                alert('Register Yourself');
-            }
+  const [email, setEmail] = useState("");
+  const [found, setFound] = useState(false);
+  const handleChange = useCallback(async () => {
+    try {
+      const response = await fetch(
+        "https://react-http-1-2a5c9-default-rtdb.firebaseio.com/movies.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await response.json();
+      const loadedMovies = [];
+      console.log(data);
+      data.array.forEach((element) => {
+        if (element.email === email) {
+          setFound(true);
         }
-        catch(error){
-            console.log(error.message);
-        }
-    },[])
-    useEffect(() => {
-
-        handleChange();
-      }, [handleChange]);
-    return (
-        <div className="auth-form-container">
-            <div id='formContainer'>
-                <div className='formContainer-div'><h2>Login</h2></div>
-                <div className='formContainer-div'>
-                    <form method='GET' action='/main'>
-                        <div className="form-group">
-                            <label for="exampleInputEmail1">Email address: </label>
-                            <input type="email" value={email} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)}></input>
-                            {/* <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> */}
-                        </div>
-                        <div className="form-group">
-                            <label for="exampleInputPassword1">Password: </label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
-                        </div>
-                        <button type="submit" className="btn btn-primary loginBTN" onSubmit={handleChange}>Log In</button>
-                    </form>
-                </div>
-            </div>
-            <div>
-                <img src={logInimg} alt="image"></img>
-            </div>
+      });
+      console.log(found);
+      if (found === false) {
+        alert("Register Yourself");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+  useEffect(() => {
+    handleChange();
+  }, [handleChange]);
+  return (
+    <div className="auth-form-container">
+      <img class="wave" src={Wave} alt="wave"/>
+      <div className="formContainer">
+        <div className="img">
+          <img src={Bg} alt="bg" />
         </div>
-    )
-}
-export default Login
+        <div className="login-content">
+          <form method="GET" action="/main">
+            <img src={Avatar} alt="avatar" />
+            <h2 className="title">Welcome</h2>
+            <div className="input-div one">
+              <div className="i">
+                <i className="fas fa-user"></i>
+              </div>
+              <div className="div">
+                <label for="exampleInputEmail1">Email address </label>
+                <input
+                  type="email"
+                  value={email}
+                  className="input"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                  onChange={(e) => setEmail(e.target.value)}
+                ></input>
+              </div>
+            </div>
+            <div class="input-div pass">
+              <div className="i">
+                <i className="fas fa-lock"></i>
+              </div>
+              <div className="div">
+                <label for="exampleInputPassword1" className="input">Password </label>
+                <input
+                  type="password"
+                  className="input"
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                ></input>
+              </div>
+            </div>
+            <a href='#'>Forgot Password</a>
+            <button
+              type="submit"
+              className="btn btn-primary loginBTN"
+              onSubmit={handleChange}
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Login;
--- Client/src/components/component-homePage/login/Register.jsx
+++ Client/src/components/component-homePage/login/Register.jsx
@@ -1,56 +1,64 @@
 import React, { useState } from "react";
 import './register.css'
 import logInimg from '../../../images/22866003-removebg-preview.png'
-import { ToastContainer, toast } from 'react-toastify'
+import axios from axios
+// import {History} from 'react-router-dom';
 const Register = (props) => {
-    const [email, setEmail] = useState('');
-    const [firstname, setFirstName] = useState('');
-    const [lastname, setLastName] = useState('');
-    const [specialization, setSpecialization] = useState('');
-    const [country, setCountry] = useState('');
-    const [from, setFrom] = useState('');
-    const [college, setCollege] = useState('');
-    const [description, setDescription] = useState('');
-    const notifyError = () => toast.error("Something went wrong");
-    const notifySuccess = () => toast.success("Application sent!");
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const items = {
-                // email: email,
-                // firstname: firstname,
-                // lastname: lastname,
-                // specialization: specialization,
-                // description: description,
-                // from: from,
-                // college: college,
-                // country: country
-                email,firstname,lastname,specialization,description,from,college,country
-            };
-            const res = await fetch("https://registration-c520f-default-rtdb.firebaseio.com/registration.json", {
-                method: "POST",
-                header: { 'Content-Type': 'application/json' },
-                // credentials: 'include',
-                body: JSON.stringify(items)
-            });
-            const data = await res.json();
-            console.log(email,firstname,lastname);
-            console.log(data[0])
-            console.log(data);
-            if (data) {
-                notifySuccess();
-            }
-        }
-        catch (error) {
-            notifyError();
-        }
-        // props.onAddItems(items);
-        // clearState();
+    // const history=useHistory();
+    const [user,setUser]=useState({
+        email:"",firstname:"",lastname:"",specialization:"",country:"",from:"",college:"",description:""
+    });
+    // const [email, setEmail] = useState('');
+    // const [firstname, setFirstName] = useState('');
+    // const [lastname, setLastName] = useState('');
+    // const [specialization, setSpecialization] = useState('');
+    // const [country, setCountry] = useState('');
+    // const [from, setFrom] = useState('');
+    // const [college, setCollege] = useState('');
+    // const [description, setDescription] = useState('');
+    let name,value;
+    const handleInput = (e) => {
+        console.log(e);
+        name=e.target.name;
+        value=e.target.value;
+        setUser({...user,[name]:value})
     };
-    // const clearState=()=>{
-    //     setFirstName('');
-    //     setEmail('');setSpecialization('');setDescription('');setFrom('');setCollege('');setCountry('');
-    // }
+    const PostData=async(e)=>{
+        e.preventDefault();
+        const {email,firstname,lastname,specialization,country,from,college,description}=user;
+        const formData=new FormData()
+        formData.append("Email",email)
+        formData.append("FirstName",firstname)
+        formData.append("LastName",lastname)
+        formData.append("Country",country)
+        formData.append("From",from)
+        formData.append("College",college)
+        formData.append("Description",description)
+        
+        // const res=await fetch('/createUser',{
+        //     method:"POST",
+        //     headers:{
+        //         "content-Type":"application/json"
+        //     },
+        //     // body:json.stringify({
+        //     //     email,firstname,lastname,specialization,country,from,college,description
+        //     // })
+        // });
+        await axios.post("http://localhost:5000/api/v1/mentorshala/createUser",formData).then((res)=>{
+            console.log(res.data);
+        }).catch((err)=>{
+            console.log(err);
+        })
+        // const data=await res.json();
+        // if(data.status===422||!data){
+        //     window.alert('Invalid Registration');
+        //     console.log('Invalid registration')
+        // }else{
+        //     window.alert(' Registration Success');
+        //     console.log('Successful registration');
+        //     // history.push('/login');
+        // }
+    };
     return (
         <div className="RegistrationContainer">
             <div className="image1_container">
@@ -59,27 +67,27 @@ const Register = (props) => {
             <div id='registerFormDiv'>
                 <div className='formContainer-div'><h2>Register</h2></div>
                 <div className='formContainer-div'>
-                    <form className="register-form" method="GET" action="/main">
+                    <form className="register-form" method="POST">
                         <label htmlFor="firstname">First name</label>
-                        <input value={firstname} onChange={(e) => setFirstName(e.target.value)} name="firstname" id="firstname" placeholder="first Name" type='text' required/>
+                        <input value={user.firstname} onChange={handleInput} name="firstname" id="firstname" placeholder="first Name" type='text' required/>
                         <label htmlFor="lastname">Last name</label>
-                        <input value={lastname} onChange={(e) => setLastName(e.target.value)} name="lastname" id="lastname" placeholder="Last Name" required/>
+                        <input value={user.lastname} onChange={handleInput} name="lastname" id="lastname" placeholder="Last Name" required/>
                         <label htmlFor="email">email</label>
-                        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" required/>
+                        <input value={user.email} onChange={handleInput} type="email" placeholder="youremail@gmail.com" id="email" name="email" required/>
                         <label htmlFor="from">From :</label>
-                        <input value={from} onChange={(e) => setFrom(e.target.value)} name="from" id="from" placeholder="From" />
+                        <input value={user.from} onChange={handleInput} name="from" id="from" placeholder="From" />
                         <label htmlFor="country">Country</label>
-                        <input value={country} onChange={(e) => setCountry(e.target.value)} name="country" id="country" placeholder="country" required />
+                        <input value={user.country} onChange={handleInput} name="country" id="country" placeholder="country" required />
                         <label htmlFor="college">College</label>
-                        <input value={from} onChange={(e) => setCollege(e.target.value)} name="college" id="college" placeholder="College" />
+                        <input value={user.college} onChange={handleInput} name="college" id="college" placeholder="college" />
                         <label htmlFor="specialization">Specialization</label>
-                        <input value={specialization} onChange={(e) => setSpecialization(e.target.value)} name="specialization" id="specialization" placeholder="Specialization" required />
+                        <input value={user.specialization} onChange={handleInput} name="specialization" id="specialization" placeholder="Specialization" required />
                         {/* <input type='file' accept="image/jpeg" onchange="uploadImage()"/> */}
                         <label htmlFor="description">Description</label>
-                        <input value={description} onChange={(e) => setDescription(e.target.value)} name="description" id="description" placeholder="Describe Yourself" />
+                        <input value={user.description} onChange={handleInput} name="description" id="description" placeholder="Describe Yourself" />
                         {/* <label htmlFor="password">password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" /> */}
-                        <button type="submit" className="btn btn-primary loginBTN" onSubmit={handleSubmit}>Register</button>
+                        <button type="submit" className="btn btn-primary loginBTN" onSubmit={PostData}>Register</button>
                     </form>
                 </div>
             </div>
