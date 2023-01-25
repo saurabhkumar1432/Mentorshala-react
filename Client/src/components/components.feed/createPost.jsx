1719dd2ba6d351d174c51be1ba992b6883d92058
Saurabh Kumar <saurabhkumar1432001@gmail.com>
2023-01-25 19:22:40 +0530

    create feed api changed to render

--- Client/src/components/components.feed/createPost.jsx
+++ Client/src/components/components.feed/createPost.jsx
@@ -26,7 +26,7 @@ const CreatePost=()=>{
         formData.append('media',media)
         formData.append('caption',caption)
         console.log(formData);
-        await axios.post('https://mentorshala-backend.up.railway.app/api/v1/mentorshala/postFeeds',formData).then((res)=>{
+        await axios.post('https://mentorshala-backend.onrender.com/api/v1/mentorshala/postFeeds',formData).then((res)=>{
             console.log(res.data);
         }).catch((err)=>{
             console.log("error");
