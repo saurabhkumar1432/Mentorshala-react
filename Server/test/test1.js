636bf323a38a38c09a00ff49b6d02df6e4ab9a97
Saurabh Kumar <saurabhkumar1432001@gmail.com>
2023-04-24 01:13:23 +0530

    testing chai mocha example

--- Server/test/test1.js
+++ Server/test/test1.js
@@ -1,13 +1,19 @@
-const assert = require("assert");
-const request = require("supertest");
-const app = require("../app");
 
-describe("GET /", function () {
-  it("responds with JSON", function (done) {
+const request = require('supertest');
+const app = require('./server');
+const expect = require('chai').expect;
+
+describe('GET /api/v1/mentorshala/getFeeds', () => {
+  it('should return a list of feeds in community', (done) => {
     request(app)
-      .get("/")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200, done);
+      .get('/getFeeds')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body[0]).to.have.property('username');
+        expect(res.body[0]).to.have.property('profile_image');
+        expect(res.body[0]).to.have.property('work');
+        done();
+      });
   });
 });
