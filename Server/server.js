c7ada4b8163b0ce252d3db6243e57e3af5ac81bf
abhay-2711 <abhaypratap.s20@iiits.in>
2023-04-23 23:52:37 +0530

    server

--- Server/server.js
+++ Server/server.js
@@ -38,11 +38,9 @@ const app=express();
 
 import cookieParser from "cookie-parser";
 // import bodyParser from "body-parser";
-import fileUpload from "express-fileupload";
+// import fileUpload from "express-fileupload";
 // import path from "path";
 import errorMiddleware from "./middleware/error.js";
-import multer from "multer";
-import fs from "fs";
 
 import fileupload from 'express-fileupload'; 
 app.use(fileupload({useTempFiles: true}))
@@ -52,15 +50,6 @@ app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 // app.use(bodyParser.urlencoded({extended:true}));
-
-app.use((req, res, next) => {
-  if (req.url === '/register') {
-    app.use(fileUpload());
-    // fileUpload()(req, res, next);
-  } else {
-    next();
-  }
-});
 
 //cors
 app.use(
@@ -78,58 +67,10 @@ app.use("/api/v1/mentorshala",userDetails)
 app.use("*",(req,res)=>{res.status(404).json("Not found")})
 
 
-import cloudinary from "cloudinary";
-import dotenv from "dotenv"
-
-//config
-if(process.env.NODE_ENV!=="PRODUCTION"){
-  dotenv.config({ path: "/config.env" });
-}
-
-cloudinary.config({
-  cloud_name: "dlw4ntoct",
-  api_key: "192653251229322",
-  api_secret: "FGk5EFFEp1Oq4tkZuNLtJy4D9EQ",
-});
-
-
-
-//multer register
-
-// Creating uploads folder if not already present
-// In "uploads" folder we will temporarily upload
-// image before uploading to cloudinary
-if (!fs.existsSync("./imgUpload")) {
-  fs.mkdirSync("./imgUpload");
-}
-
-const upload = multer({ dest: "./imgUpload/" });
-
-app.post("/register1",upload.single('avatar'),async (req, res, next) => {
-  // req.file is the `profile-file` file
-  // req.body will hold the text fields,
-  // if there were any
-    if(req.file)
-    console.log(req.file.path);
-    else
-    console.log("no file uploaded");
-    if (req.fileValidationError) {
-      return res.status(400).send({ error: req.fileValidationError });
-    }
-    if (!req.file) {
-      return res.status(400).send({ error: "Please select an image to upload" });
-    }
-}
-);
-
 
 // const server = httpServer.listen(process.env.PORT,()=>{
 //     console.log("Server is working on : ", process.env.PORT);
 // })
-
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Server is working on http://localhost:${process.env.PORT}`);
-});
 
 //Unhandled Promise Rejection
 process.on("unhandledRejection", (err) => {
